1f52b17e8a82c69850e7906e3c95e5736e0cdc7d
VWA Software internet <henk@vwa.nu>
2019-05-27 10:46:21 +0200

    Etc Connector client files

---
+++ src/Umbraco.Web.UI/App_Plugins/ETCConnector/BackOffice/ETCConnector/settings.controller.js
@@ -0,0 +1,30 @@
+﻿angular.module("umbraco").controller("ETC.Connector.SettingsController", ['$scope', '$http', 'notificationsService', function ($scope, $http, notificationsService) {
+
+
+	$scope.Data = {};
+	$scope.contentForm.$dirty = false;
+
+	$http.get('backoffice/ETCConnector/Spider/GetGlobalSettings/',
+		{
+			cache: false
+		}).then(function (response) {
+			$scope.Data = response.data;
+		}, function (err) {
+			notificationsService.error("oops :" + err.data.ExceptionMessage, err.ExceptionMessage);
+		});
+
+	$scope.Commit = function () {
+		$http.post('backoffice/ETCConnector/Spider/SaveGlobalSettings/', $scope.Data).then(function (res) {
+			
+			$scope.Data = res.data;
+			$scope.contentForm.$dirty = false;
+			notificationsService.success("Document Published", "Gelukt, de wijzigingen zijn succesvol opgelagen");
+
+		}, function (err) {
+			notificationsService.error("Niet opgeslagen", err);
+		});
+		return false;
+
+	};
+	
+}]);
